e02b8808150b98a5922402ec125571a9cfd57072
Christian Venegas <venegaschristian014@gmail.com>
2025-11-04 21:41:39 -0600

    Se implemento las clases principales del algoritmo

--- gen/algorithm.py
+++ gen/algorithm.py
@@ -5,3 +5,13 @@
 # 2. Seleccionar a los padres
 # 3. Crossover y mutación
 # 4. Regresa al paso 1
+from .population import Population
+# from .individual import Individual
+
+INDIVIDUAL_CHROMOSOME_SIZE = 50
+POPULATION_SIZE = 50
+
+population = Population(POPULATION_SIZE, INDIVIDUAL_CHROMOSOME_SIZE)
+
+for individual in population.individuals:
+    print(individual.chromosome)
--- gen/population.py
+++ gen/population.py
@@ -4,11 +4,16 @@ from .individual import Individual
 # Representación de una población.
 class Population:
     # Los parametros iniciales que se le van a pasar son el tamaño de la población y el tamaño del cromosoma de cada individuo.
-    def __init__(self, size_population: int, chromosome_size: int):
-        self.size_population = size_population
-        self.chromosome_size = chromosome_size
-        self.individuals = []
-
+    def __init__(
+        self, size_population: int, chromosome_size: int, generations: int = 50
+    ):
+        self.size_population: int = size_population
+        self.chromosome_size: int = chromosome_size
+        # Generar una población inicial (1ejecución del programa = 50 generaciones = 50 juegos con información de la población anterior)
+        self.generations: int = generations
+        # Número de generacion actual.
+        self.actual_generation: int = 1
         # Asignación de los individuos de la población.
-        for index in range(self.size_population):
-            self.individuals[index] = Individual(self.chromosome_size)
+        self.individuals: list[Individual] = [
+            Individual(self.chromosome_size) for _ in range(self.size_population)
+        ]
